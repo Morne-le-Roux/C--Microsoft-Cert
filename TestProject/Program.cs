b926185b8b84b3f1a7edfef93579ce92050b9f78
Morné le Roux <mornelerx@gmail.com>
2023-11-17 19:12:56 +0200

    init code

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,10 +1,7 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
 
-Console.WriteLine($"First Roll: {roll1}");
-Console.WriteLine($"Second Roll {roll2}");
-Console.WriteLine($"Third Roll {roll3}");
+largerValue = System.Math.Max(firstValue, secondValue);
 
-dice.Next()
+Console.WriteLine(largerValue);
