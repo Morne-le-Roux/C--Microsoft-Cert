08d0a9ece2ebb8c8541d3d1e913d012b24c72d8a
Morné le Roux <mornelerx@gmail.com>
2023-11-17 19:06:55 +0200

    init code

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First Roll: {roll1}");
+Console.WriteLine($"Second Roll {roll2}");
+Console.WriteLine($"Third Roll {roll3}");
+
+dice.Next()
